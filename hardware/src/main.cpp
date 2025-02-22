7aa32e224f511808984e4bcd326e114d61e8841c
Jorge Pérez <cotepv25@gmail.com>
2025-02-22 10:15:19 +0100

    main hardware code init

--- hardware/src/main.cpp
+++ hardware/src/main.cpp
@@ -1,18 +1,47 @@
-#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define SCREEN_WIDTH 128  //screen width
+#define SCREEN_HEIGHT 64  //screen height
+#define OLED_RESET    -1  // Reset 
+#define SCREEN_ADDRESS 0x3C  //I2C direction
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  int button1_pin = 13;
+    Wire.begin(21, 22); // SDA=GPIO21, SCL=GPIO22
+    Serial.begin(115200);
+    int pagina = 0;
+
+    if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+        Serial.println(F("Error al inicializar la pantalla OLED"));
+        for (;;);
+    }
+
+    display.clearDisplay();
+    display.setTextSize(.5);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 10);
+    display.println("Hola, ESP32!");
+    display.println("Hola, ESP32!");
+    if (pagina == 0){
+      display.clearDisplay();
+      display.setTextSize(1.7);
+      display.setTextColor(WHITE);
+      display.setCursor(0, 10);
+      display.println();
+      display.println("SKILL TRACKER");
+    }
+
+
+    
+    display.display(); // Mostrar en pantalla
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+ 
+    
+  
 }
